7e7f0c68a8cdcd20733116db74f3ce7847990162
Ceri Bain <ceribain@Ceris-MacBook-Pro.local>
2026-04-06 17:45:50 +0100

    Add movies array to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,11 @@
 function Home() {
+  const movies = [
+    { id: 1, title: "Dark Knight", release_date: "2008" },
+    { id: 2, title: "Oppenheimer", release_date: "2023" },
+    { id: 3, title: "Tenet", release_date: "2020" },
+    { id: 4, title: "1917 ", release_date: "2019" },
+    { id: 5, title: "The Social Network", release_date: "2010" },
+  ];
+
   return <div className="home"></div>;
 }
