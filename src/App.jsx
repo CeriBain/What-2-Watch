c6ce858c024e7aa82d476994b3a546e53affdfda
Ceri Bain <ceribain@Ceris-MacBook-Pro.local>
2026-04-06 17:24:04 +0100

    Update app.jsx with component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import MovieCard from "./components/MovieCard";
+import "./App.css";
 
 function App() {
-
   return (
-    <> 
-
+    <>
+      <div>
+        <MovieCard movie={{ title: "Dark Knight", release_date: "2008" }} />
+      </div>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
